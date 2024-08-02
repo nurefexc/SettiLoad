dc698b120a55fbe60d73c4cb61ec6ed3a0fd2cad
nurefexc <nurefexc@outlook.com>
2024-08-02 10:40:39 +0200

    Xml load
    * [IMP] Add XML files loading option
    
    * [IMP] Improvement of the test based on the new xml loading function

--- SettiLoad/Config.cs
+++ SettiLoad/Config.cs
@@ -1,86 +1,200 @@
 ﻿using System.Reflection;
 using System.Text.Json;
+using System.Xml;
+using System.Xml.Serialization;
 
-namespace SettiLoad;
-
-/// <summary>
-/// An abstract base class for creating configurations that can be loaded from a configuration file.
-/// </summary>
-public abstract class Config
+namespace SettiLoad
 {
     /// <summary>
-    /// Loads the configuration from the specified file.
+    /// An abstract base class for creating configurations that can be loaded from a configuration file.
+    /// Supports loading from JSON and XML files.
     /// </summary>
-    /// <param name="filePath">The path to the configuration file.</param>
-    /// <param name="throwException">Indicates whether to throw an exception if loading fails.</param>
-    /// <returns>Returns true if the configuration was successfully loaded; otherwise, returns false.</returns>
-    public bool Load(string filePath, bool throwException=false)
+    public abstract class Config
     {
-        try
+        /// <summary>
+        /// Loads the configuration from the specified file.
+        /// Supported file types: JSON (.json) and XML (.xml).
+        /// </summary>
+        /// <param name="filePath">The path to the configuration file.</param>
+        /// <param name="throwException">Indicates whether to throw an exception if loading fails.</param>
+        /// <returns>Returns true if the configuration was successfully loaded; otherwise, returns false.</returns>
+        public bool Load(string filePath, bool throwException = false)
         {
-            // Read the entire text content from the specified JSON file.
-            var json = File.ReadAllText(filePath);
-
-            // Parse the JSON text into a JsonDocument.
-            var jsonDoc = JsonDocument.Parse(json);
-
-            // Apply settings from the JSON document to this configuration object.
-            ApplySettings(this, jsonDoc.RootElement);
-        }
-        catch (Exception)
-        {
-            if (throwException)
+            try
             {
-                // Rethrow the exception if specified.
-                throw;
-            }
-            // Return false if an exception occurred and throwException is false.
-            return false;
-        }
+                var fileExtension = Path.GetExtension(filePath).ToLower();
+                bool success = false;
 
-        // Return true to indicate that the configuration was successfully loaded.
-        return true;
-    }
-
-    /// <summary>
-    /// Recursively applies settings from a JSON element to the corresponding properties of the configuration object.
-    /// </summary>
-    /// <param name="propertyConfig">The configuration object whose properties are to be set.</param>
-    /// <param name="jsonElement">The JSON element containing the settings to be applied.</param>
-    private static void ApplySettings(object propertyConfig, JsonElement jsonElement)
-    {
-        // Iterate over each property in the JSON element.
-        foreach (var property in jsonElement.EnumerateObject())
-        {
-            // Get the property information of the configuration object that matches the current JSON property name.
-            var propInfo = propertyConfig.GetType().GetProperty(property.Name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
-
-            // Check if the property exists and is a class (excluding strings), indicating that it might have nested properties.
-            if (propInfo != null && propInfo.PropertyType.IsClass && propInfo.PropertyType != typeof(string))
-            {
-                // Create an instance of the nested configuration class.
-                var nestedConfig = Activator.CreateInstance(propInfo.PropertyType);
-                
-                // If the instance creation is successful, apply settings to this nested configuration object.
-                if (nestedConfig != null)
+                if (fileExtension == ".json")
                 {
-                    ApplySettings(nestedConfig, property.Value);
+                    // Attempt to load as JSON.
+                    success = TryLoadJson(filePath);
+                }
+                else if (fileExtension == ".xml")
+                {
+                    // Attempt to load as XML.
+                    success = TryLoadXml(filePath);
+                }
+                else
+                {
+                    // Attempt both JSON and XML.
+                    success = TryLoadJson(filePath) || TryLoadXml(filePath);
+                }
 
-                    // Set the nested configuration object to the property of the parent configuration object.
-                    propInfo.SetValue(propertyConfig, nestedConfig);
+                if (!success)
+                {
+                    throw new NotSupportedException("The configuration file format is not supported or could not be parsed.");
+                }
+
+                return true;
+            }
+            catch (Exception)
+            {
+                if (throwException)
+                {
+                    // Rethrow the exception if specified.
+                    throw;
+                }
+                // Return false if an exception occurred and throwException is false.
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Tries to load the configuration from a JSON file.
+        /// </summary>
+        /// <param name="filePath">The path to the JSON configuration file.</param>
+        /// <returns>Returns true if the configuration was successfully loaded; otherwise, returns false.</returns>
+        private bool TryLoadJson(string filePath)
+        {
+            try
+            {
+                // Read the entire text content from the specified JSON file.
+                var json = File.ReadAllText(filePath);
+
+                // Parse the JSON text into a JsonDocument.
+                var jsonDoc = JsonDocument.Parse(json);
+
+                // Apply settings from the JSON document to this configuration object.
+                ApplyJsonSettings(this, jsonDoc.RootElement);
+
+                return true;
+            }
+            catch
+            {
+                // Return false if parsing or applying settings failed.
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Tries to load the configuration from an XML file.
+        /// </summary>
+        /// <param name="filePath">The path to the XML configuration file.</param>
+        /// <returns>Returns true if the configuration was successfully loaded; otherwise, returns false.</returns>
+        private bool TryLoadXml(string filePath)
+        {
+            try
+            {
+                // Read the entire text content from the specified XML file.
+                var xml = File.ReadAllText(filePath);
+
+                // Parse the XML text into an XmlDocument.
+                var xmlDoc = new XmlDocument();
+                xmlDoc.LoadXml(xml);
+
+                // Apply settings from the XML document to this configuration object.
+                ApplyXmlSettings(this, xmlDoc.DocumentElement);
+
+                return true;
+            }
+            catch
+            {
+                throw;
+                // Return false if parsing or applying settings failed.
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Recursively applies settings from a JSON element to the corresponding properties of the configuration object.
+        /// </summary>
+        /// <param name="propertyConfig">The configuration object whose properties are to be set.</param>
+        /// <param name="jsonElement">The JSON element containing the settings to be applied.</param>
+        private static void ApplyJsonSettings(object propertyConfig, JsonElement jsonElement)
+        {
+            // Iterate over each property in the JSON element.
+            foreach (var property in jsonElement.EnumerateObject())
+            {
+                // Get the property information of the configuration object that matches the current JSON property name.
+                var propInfo = propertyConfig.GetType().GetProperty(property.Name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+
+                // Check if the property exists and is a class (excluding strings), indicating that it might have nested properties.
+                if (propInfo != null && propInfo.PropertyType.IsClass && propInfo.PropertyType != typeof(string))
+                {
+                    // Create an instance of the nested configuration class.
+                    var nestedConfig = Activator.CreateInstance(propInfo.PropertyType);
+
+                    // If the instance creation is successful, apply settings to this nested configuration object.
+                    if (nestedConfig != null)
+                    {
+                        ApplyJsonSettings(nestedConfig, property.Value);
+
+                        // Set the nested configuration object to the property of the parent configuration object.
+                        propInfo.SetValue(propertyConfig, nestedConfig);
+                    }
+                }
+                // If the property exists and is not a class (or is a string), set its value directly.
+                else if (propInfo != null)
+                {
+                    // Get the raw JSON text of the property value.
+                    var value = property.Value.GetRawText();
+
+                    // Deserialize the JSON text into the appropriate property type.
+                    var typedValue = JsonSerializer.Deserialize(value, propInfo.PropertyType);
+
+                    // Set the deserialized value to the property of the configuration object.
+                    propInfo.SetValue(propertyConfig, typedValue);
                 }
             }
-            // If the property exists and is not a class (or is a string), set its value directly.
-            else if (propInfo != null)
+        }
+
+        /// <summary>
+        /// Recursively applies settings from an XML element to the corresponding properties of the configuration object.
+        /// </summary>
+        /// <param name="propertyConfig">The configuration object whose properties are to be set.</param>
+        /// <param name="xmlElement">The XML element containing the settings to be applied.</param>
+        private static void ApplyXmlSettings(object propertyConfig, XmlElement xmlElement)
+        {
+            // Iterate over each child node in the XML element.
+            foreach (XmlNode node in xmlElement.ChildNodes)
             {
-                // Get the raw JSON text of the property value.
-                var value = property.Value.GetRawText();
+                if (node is XmlElement element)
+                {
+                    // Get the property information of the configuration object that matches the current XML element name.
+                    var propInfo = propertyConfig.GetType().GetProperty(element.Name, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
 
-                // Deserialize the JSON text into the appropriate property type.
-                var typedValue = JsonSerializer.Deserialize(value, propInfo.PropertyType);
+                    // Check if the property exists and is a class (excluding strings), indicating that it might have nested properties.
+                    if (propInfo != null && propInfo.PropertyType.IsClass && propInfo.PropertyType != typeof(string))
+                    {
+                        // Create an instance of the nested configuration class.
+                        var nestedConfig = Activator.CreateInstance(propInfo.PropertyType);
 
-                // Set the deserialized value to the property of the configuration object.
-                propInfo.SetValue(propertyConfig, typedValue);
+                        // If the instance creation is successful, apply settings to this nested configuration object.
+                        if (nestedConfig != null)
+                        {
+                            ApplyXmlSettings(nestedConfig, element);
+
+                            // Set the nested configuration object to the property of the parent configuration object.
+                            propInfo.SetValue(propertyConfig, nestedConfig);
+                        }
+                    }
+                    // If the property exists and is not a class (or is a string), set its value directly.
+                    else if (propInfo != null)
+                    {
+                        propInfo.SetValue(propertyConfig, Convert.ChangeType(element.InnerText, propInfo.PropertyType));
+                    }
+                }
             }
         }
     }
--- SettiLoadTest/UnitTest1.cs
+++ SettiLoadTest/UnitTest1.cs
@@ -1,138 +1,166 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
 namespace SettiLoad.Tests
 {
     [TestFixture]
     public class ConfigTests
     {
-        // Paths to the configuration files used in the tests
-        private string appConfigFilePath;
-        private string alternativeConfigFilePath;
-        private string invalidJsonFilePath;
-        private string partialConfigFilePath;
-        private string missingConfigFilePath;
-        private string typeTestConfigFilePath;
+        public enum FileType
+        {
+            Json,
+            Xml
+        }
+
+        public enum TestType
+        {
+            AppConfig,
+            AlternativeConfig,
+            InvalidConfig,
+            PartialConfig,
+            MissingConfig,
+            TypeTestConfig
+        }
+
+        
+        private Dictionary<TestType, Dictionary<FileType, string>> filePaths;
 
         [SetUp]
         public void SetUp()
         {
-            // Get the path to the current test directory
             var testDirectory = TestContext.CurrentContext.TestDirectory;
-            
-            // Set paths to configuration files located in the TestConfigs directory
-            appConfigFilePath = Path.Combine(testDirectory, "TestConfigs", "appConfig.json");
-            alternativeConfigFilePath = Path.Combine(testDirectory, "TestConfigs", "alternativeConfig.json");
-            invalidJsonFilePath = Path.Combine(testDirectory, "TestConfigs", "invalidConfig.json");
-            partialConfigFilePath = Path.Combine(testDirectory, "TestConfigs", "partialConfig.json");
-            missingConfigFilePath = Path.Combine(testDirectory, "TestConfigs", "missingConfig.json");
-            typeTestConfigFilePath = Path.Combine(testDirectory, "TestConfigs", "typeTestConfig.json");
+
+            filePaths = new Dictionary<TestType, Dictionary<FileType, string>>
+            {
+                [TestType.AppConfig] = new Dictionary<FileType, string>
+                {
+                    { FileType.Json, Path.Combine(testDirectory, "TestConfigs", "json", "appConfig.json") },
+                    { FileType.Xml, Path.Combine(testDirectory, "TestConfigs", "xml", "appConfig.xml") }
+                },
+                [TestType.AlternativeConfig] = new Dictionary<FileType, string>
+                {
+                    { FileType.Json, Path.Combine(testDirectory, "TestConfigs", "json", "alternativeConfig.json") },
+                    { FileType.Xml, Path.Combine(testDirectory, "TestConfigs", "xml", "alternativeConfig.xml") }
+                },
+                [TestType.InvalidConfig] = new Dictionary<FileType, string>
+                {
+                    { FileType.Json, Path.Combine(testDirectory, "TestConfigs", "json", "invalidConfig.json") },
+                    { FileType.Xml, Path.Combine(testDirectory, "TestConfigs", "xml", "invalidConfig.xml") }
+                },
+                [TestType.PartialConfig] = new Dictionary<FileType, string>
+                {
+                    { FileType.Json, Path.Combine(testDirectory, "TestConfigs", "json", "partialConfig.json") },
+                    { FileType.Xml, Path.Combine(testDirectory, "TestConfigs", "xml", "partialConfig.xml") }
+                },
+                [TestType.MissingConfig] = new Dictionary<FileType, string>
+                {
+                    { FileType.Json, Path.Combine(testDirectory, "TestConfigs", "json", "missingConfig.json") },
+                    { FileType.Xml, Path.Combine(testDirectory, "TestConfigs", "xml", "missingConfig.xml") }
+                },
+                [TestType.TypeTestConfig] = new Dictionary<FileType, string>
+                {
+                    { FileType.Json, Path.Combine(testDirectory, "TestConfigs", "json", "typeTestConfig.json") },
+                    { FileType.Xml, Path.Combine(testDirectory, "TestConfigs", "xml", "typeTestConfig.xml") }
+                }
+            };
         }
 
-        [Test]
-        public void Load_ShouldLoadAppConfigSettingsFromJson()
+        private void LoadAndVerifyConfig(TestType configType, FileType fileType, Action<AppConfig> verifyConfig)
         {
-            // Create a new instance of AppConfig and load settings from the appConfig.json file
             var config = new AppConfig();
-            var result = config.Load(appConfigFilePath);
+            var result = config.Load(filePaths[configType][fileType], true);
 
-            // Verify that the load operation was successful
             Assert.IsTrue(result);
-
-            // Verify that the loaded configuration matches expected values
-            Assert.IsNotNull(config.Database);
-            Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", config.Database.ConnectionString);
-            Assert.AreEqual(30, config.Database.Timeout);
-            Assert.IsNotNull(config.Logging);
-            Assert.AreEqual("Info", config.Logging.LogLevel);
-            Assert.AreEqual("/var/log/app.log", config.Logging.LogFilePath);
-            Assert.IsTrue(config.IsFeatureEnabled);
+            verifyConfig(config);
         }
 
-        [Test]
-        public void Load_ShouldLoadAlternativeConfigSettingsFromJson()
+        [TestCase(TestType.AppConfig, FileType.Json)]
+        [TestCase(TestType.AppConfig, FileType.Xml)]
+        public void Load_ShouldLoadAppConfigSettings(TestType configType, FileType fileType)
         {
-            // Create a new instance of AppConfig and load settings from the alternativeConfig.json file
-            var config = new AppConfig();
-            var result = config.Load(alternativeConfigFilePath);
-
-            // Verify that the load operation was successful
-            Assert.IsTrue(result);
-
-            // Verify that the loaded configuration matches expected values for the alternative configuration
-            Assert.IsNotNull(config.Database);
-            Assert.AreEqual("Server=anotherServerAddress;Database=anotherDataBase;User Id=anotherUsername;Password=anotherPassword;", config.Database.ConnectionString);
-            Assert.AreEqual(60, config.Database.Timeout);
-            Assert.IsNotNull(config.Logging);
-            Assert.AreEqual("Debug", config.Logging.LogLevel);
-            Assert.AreEqual("/var/log/anotherapp.log", config.Logging.LogFilePath);
-            Assert.IsFalse(config.IsFeatureEnabled);
+            LoadAndVerifyConfig(configType, fileType, config =>
+            {
+                Assert.IsNotNull(config.Database);
+                Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", config.Database.ConnectionString);
+                Assert.AreEqual(30, config.Database.Timeout);
+                Assert.IsNotNull(config.Logging);
+                Assert.AreEqual("Info", config.Logging.LogLevel);
+                Assert.AreEqual("/var/log/app.log", config.Logging.LogFilePath);
+                Assert.IsTrue(config.IsFeatureEnabled);
+            });
         }
 
-        [Test]
-        public void Load_ShouldHandleInvalidJsonGracefully()
+        [TestCase(TestType.AlternativeConfig, FileType.Json)]
+        [TestCase(TestType.AlternativeConfig, FileType.Xml)]
+        public void Load_ShouldLoadAlternativeConfigSettings(TestType configType, FileType fileType)
         {
-            // Create a new instance of AppConfig and load settings from the invalidConfig.json file
-            var config = new AppConfig();
-            var result = config.Load(invalidJsonFilePath);
+            LoadAndVerifyConfig(configType, fileType, config =>
+            {
+                Assert.IsNotNull(config.Database);
+                Assert.AreEqual("Server=anotherServerAddress;Database=anotherDataBase;User Id=anotherUsername;Password=anotherPassword;", config.Database.ConnectionString);
+                Assert.AreEqual(60, config.Database.Timeout);
+                Assert.IsNotNull(config.Logging);
+                Assert.AreEqual("Debug", config.Logging.LogLevel);
+                Assert.AreEqual("/var/log/anotherapp.log", config.Logging.LogFilePath);
+                Assert.IsFalse(config.IsFeatureEnabled);
+            });
+        }
 
-            // Verify that the load operation returns false due to invalid JSON
+        [TestCase(TestType.InvalidConfig, FileType.Json)]
+        [TestCase(TestType.InvalidConfig, FileType.Xml)]
+        public void Load_ShouldHandleInvalidConfigGracefully(TestType configType, FileType fileType)
+        {
+            var config = new AppConfig();
+            var result = config.Load(filePaths[configType][fileType]);
+
             Assert.IsFalse(result);
-
-            // Verify that no settings were loaded due to invalid JSON
             Assert.IsNull(config.Database);
             Assert.IsNull(config.Logging);
             Assert.IsFalse(config.IsFeatureEnabled);
         }
 
-        [Test]
-        public void Load_ShouldHandleMissingFieldsInJson()
+        [TestCase(TestType.PartialConfig, FileType.Json)]
+        [TestCase(TestType.PartialConfig, FileType.Xml)]
+        public void Load_ShouldHandleMissingFieldsInConfig(TestType configType, FileType fileType)
         {
-            // Create a new instance of AppConfig and load settings from the partialConfig.json file
-            var config = new AppConfig();
-            var result = config.Load(partialConfigFilePath);
-
-            // Verify that the load operation was successful despite missing fields
-            Assert.IsTrue(result);
-
-            // Verify that the loaded configuration has the fields that were present
-            Assert.IsNotNull(config.Database);
-            Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", config.Database.ConnectionString);
-            Assert.IsNull(config.Logging.LogLevel); // Logging should be null as it was not specified in the JSON
-            Assert.IsFalse(config.IsFeatureEnabled); // Default value is false if not specified
+            LoadAndVerifyConfig(configType, fileType, config =>
+            {
+                Assert.IsNotNull(config.Database);
+                Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", config.Database.ConnectionString);
+                Assert.IsNull(config.Logging.LogLevel); // Logging should be null as it was not specified in the config
+                Assert.IsFalse(config.IsFeatureEnabled); // Default value is false if not specified
+            });
         }
 
-        [Test]
-        public void Load_ShouldReturnFalseWhenConfigFileIsMissing()
+        [TestCase(TestType.MissingConfig, FileType.Json)]
+        [TestCase(TestType.MissingConfig, FileType.Xml)]
+        public void Load_ShouldReturnFalseWhenConfigFileIsMissing(TestType configType, FileType fileType)
         {
-            // Create a new instance of AppConfig and attempt to load settings from a missing configuration file
             var config = new AppConfig();
-            var result = config.Load(missingConfigFilePath);
+            var result = config.Load(filePaths[configType][fileType]);
 
-            // Verify that the load operation returns false as the file is missing
             Assert.IsFalse(result);
-
-            // Verify that no settings were loaded due to the missing file
             Assert.IsNull(config.Database);
             Assert.IsNull(config.Logging);
             Assert.IsFalse(config.IsFeatureEnabled);
         }
 
-        [Test]
-        public void Load_ShouldHandleDifferentJsonDataTypes()
+        [TestCase(TestType.TypeTestConfig, FileType.Json)]
+        [TestCase(TestType.TypeTestConfig, FileType.Xml)]
+        public void Load_ShouldHandleDifferentDataTypes(TestType configType, FileType fileType)
         {
-            // Create a new instance of AppConfig and load settings from the typeTestConfig.json file
-            var config = new AppConfig();
-            var result = config.Load(typeTestConfigFilePath);
-
-            // Verify that the load operation was successful
-            Assert.IsTrue(result);
-
-            // Verify that the loaded configuration matches expected values with different data types
-            Assert.IsNotNull(config.Database);
-            Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", config.Database.ConnectionString);
-            Assert.AreEqual(45, config.Database.Timeout);
-            Assert.IsNotNull(config.Logging);
-            Assert.AreEqual("Verbose", config.Logging.LogLevel);
-            Assert.AreEqual("/var/log/complex.log", config.Logging.LogFilePath);
-            Assert.IsTrue(config.IsFeatureEnabled);
+            LoadAndVerifyConfig(configType, fileType, config =>
+            {
+                Assert.IsNotNull(config.Database);
+                Assert.AreEqual("Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;", config.Database.ConnectionString);
+                Assert.AreEqual(45, config.Database.Timeout);
+                Assert.IsNotNull(config.Logging);
+                Assert.AreEqual("Verbose", config.Logging.LogLevel);
+                Assert.AreEqual("/var/log/complex.log", config.Logging.LogFilePath);
+                Assert.IsTrue(config.IsFeatureEnabled);
+            });
         }
     }
 }
